08765aa94f0654a542558bc2b0c20fbabf5edc4e
lyzamaemirabete <lyza.mirabete@boom.camp>
2020-01-03 14:01:38 +0800

    Node-2

---
+++ server/controllers/userController.js
@@ -0,0 +1,18 @@
+signUp = (req, res) => {
+  const db = req.app.get("db");
+
+  const { email, password } = req.body;
+  const newUser = { id: db.users.id, password, email };
+
+  db.users.data.push(newUser);
+  db.users.id++;
+
+  db.profiles.data.push({ userId: newUser.id, thumbnail: null, about: "" });
+  db.profiles.id++;
+
+  res.status(200).json(newUser);
+};
+
+module.exports = {
+  signUp
+};
